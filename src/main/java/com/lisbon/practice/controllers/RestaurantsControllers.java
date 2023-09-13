6404c9b18ce106851ebf8cb884d6a0a84e3ee8e1
Your Name <you@example.com>
2023-09-13 16:31:24 +0200

    added controller and service class for restaurant class

--- src/main/java/com/lisbon/practice/controllers/RestaurantsControllers.java
+++ src/main/java/com/lisbon/practice/controllers/RestaurantsControllers.java
@@ -1,13 +1,52 @@
 package com.lisbon.practice.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lisbon.practice.model.RESTAURANTS;
 import com.lisbon.practice.services.RestaurantService;
 
 @RestController
 public class RestaurantsControllers {
 
 	@Autowired
-	private RestaurantService restsurantServive;
+	private RestaurantService restaurantService;
+	
+	@GetMapping("/restaurants")
+	public List<RESTAURANTS> getRestaurants()
+	{
+		return restaurantService.getRestaurants();
+		
+	}
+	
+	@PostMapping("/addRestaurant")
+	public RESTAURANTS newRestaurant(@RequestBody RESTAURANTS restaurants, @PathVariable long restaurant_id)
+	{
+		return restaurantService.addRestaurant(restaurants);
+		
+	}
+	
+	@PutMapping("/updateRestaurabt/{restaurant_id}")
+	public RESTAURANTS updateRestaurant(@RequestBody RESTAURANTS restaurant, @PathVariable long restaurant_id)
+	{
+		return restaurantService.updateRestuarant(restaurant);
+		
+	}
+	
+	@DeleteMapping("/deleteRestaurant/{restaurant_id}")
+	public String deleteRestaurantById(@PathVariable long restaurant_id)
+	{
+		
+		restaurantService.deleteRestuarant(restaurant_id);
+		return "the restaurant record has been deleted successfully";
+		
+	}
 }
--- src/main/java/com/lisbon/practice/services/RestaurantService.java
+++ src/main/java/com/lisbon/practice/services/RestaurantService.java
@@ -1,8 +1,11 @@
 package com.lisbon.practice.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.lisbon.practice.model.RESTAURANTS;
 import com.lisbon.practice.respositories.RestaurantsRepository;
 
 @Service
@@ -10,4 +13,24 @@ public class RestaurantService {
 
 	@Autowired
 	private RestaurantsRepository restaurantsRepository;
+
+	public List<RESTAURANTS> getRestaurants() {
+		// TODO Auto-generated method stub
+		return restaurantsRepository.findAll();
+	}
+
+	public RESTAURANTS addRestaurant(RESTAURANTS restaurants) {
+		// TODO Auto-generated method stub
+		return restaurantsRepository.save(restaurants);
+	}
+
+	public RESTAURANTS updateRestuarant(RESTAURANTS restaurant) {
+		// TODO Auto-generated method stub
+		return restaurantsRepository.save(restaurant);
+	}
+
+	public void deleteRestuarant(long restaurant_id) {
+		// TODO Auto-generated method stub
+		restaurantsRepository.deleteById(restaurant_id);
+	}
 }
