2d2e163712ed4690f76f06055d4cec23b0c70b89
Your Name <you@example.com>
2023-09-13 16:08:41 +0200

    added service and controller classes of entity users

--- src/main/java/com/lisbon/practice/controllers/UsersController.java
+++ src/main/java/com/lisbon/practice/controllers/UsersController.java
@@ -1,14 +1,53 @@
 package com.lisbon.practice.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lisbon.practice.model.USERS;
 import com.lisbon.practice.services.UsersService;
 
 @RestController
 public class UsersController {
 	
 	@Autowired
-	private UsersService usersServive;
+	private UsersService usersService;
+	
+	@GetMapping("/users")
+	public List<USERS> getAllUsers(){
+		
+		return usersService.fetchAllUsers();
+	}
+	
+	@PostMapping("/saveUser")
+	public USERS addUsers(@Validated @RequestBody USERS user) {
+		
+		return usersService.createUser(user);
+		
+	}
+	
+	@PutMapping("/updateUsers/{users_id}")
+	public USERS editUser(@RequestBody USERS user, @PathVariable Long users_id)
+	{
+		return usersService.updateUser(user, users_id);
+		
+	}
+	
+	@DeleteMapping("deleteUser/{users_id}")
+	public String deleteUsers(@PathVariable Long users_id)
+	{
+		usersService.deleteUserById(users_id);
+		return "successfully deleted user";
+		
+	}
+	
 
 }
--- src/main/java/com/lisbon/practice/services/UsersService.java
+++ src/main/java/com/lisbon/practice/services/UsersService.java
@@ -1,8 +1,11 @@
 package com.lisbon.practice.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.lisbon.practice.model.USERS;
 import com.lisbon.practice.respositories.UsersRepository;
 
 @Service
@@ -10,4 +13,25 @@ public class UsersService {
 
 	@Autowired
 	private UsersRepository usersRepository;
+
+	public List<USERS> fetchAllUsers() {
+		// TODO Auto-generated method stub
+		return usersRepository.findAll();
+	}
+
+	public USERS createUser(USERS user) {
+		// TODO Auto-generated method stub
+		return usersRepository.save(user);
+	}
+
+	public USERS updateUser(USERS user, Long users_id) {
+		// TODO Auto-generated method stub
+		return usersRepository.save(user);
+	}
+
+	public void deleteUserById(Long users_id) {
+		// TODO Auto-generated method stub
+		usersRepository.deleteById(users_id);
+	}
+	
 }
